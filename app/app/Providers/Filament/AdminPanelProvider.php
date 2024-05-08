6fb60ebf3d78372bec72f29c616d459dfa63406f
Anatolii Iumashev <anatolii.iumashev@gmail.com>
2024-05-08 17:25:55 +0300

    root as route

--- app/app/Providers/Filament/AdminPanelProvider.php
+++ app/app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            // ->path('a')
             ->login()
             ->colors([
                 'primary' => Color::Amber,
